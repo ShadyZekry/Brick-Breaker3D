9e4582b1f0b651d6f07c908358f390bbc5b4ad4e
Shady <ShadyZekry75@gmail.com>
2021-06-08 00:44:02 +0200

    Add breaking script for boxes

--- Assets/Scripts/Box_Break.cs
+++ Assets/Scripts/Box_Break.cs
@@ -2,14 +2,22 @@ using UnityEngine;
 
 public class Box_Break : MonoBehaviour
 {
+    [SerializeField]
+    GameObject damagedBoxPrefab;
     void Start()
     {
-        Renderer rend = GetComponent<Renderer>();
-        rend.material.SetTextureOffset("_MainTex", new Vector2(Random.Range(-10, 10), Random.Range(-10, 10)));
+        // Renderer rend = GetComponent<Renderer>();
+        // rend.material.SetTextureOffset("_MainTex", new Vector2(Random.Range(-10, 10), Random.Range(-10, 10)));
     }
 
-    void Update()
+    void OnCollisionEnter(Collision collision)
     {
+        if (collision.collider.name != "ball" && gameObject.name != "BrokernBox") return;
 
+        Destroy(gameObject);
+        GameObject gobj = GetComponent<GameObject>();
+        gobj = Instantiate(damagedBoxPrefab, transform.position, Quaternion.identity, transform.parent);
+        Renderer rend = GetComponent<Renderer>();
+        // rend.material.SetTextureOffset("_MainTex", new Vector2(Random.Range(-10, 10), Random.Range(-10, 10)));
     }
 }
