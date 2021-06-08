298602aad76e9571ba94d191a50006f6c2d974f1
Shady <ShadyZekry75@gmail.com>
2021-06-08 02:51:42 +0200

    Spawn rows every 10 seconds

--- Assets/Scripts/Spawn_Boxes_Row.cs
+++ Assets/Scripts/Spawn_Boxes_Row.cs
@@ -9,23 +9,23 @@ public class Spawn_Boxes_Row : MonoBehaviour
     List<float> availablePositions;
     void Start()
     {
-        // InvokeRepeating("SpawnRandom", 3f, 3f);
-        SpawnRandom();
+        InvokeRepeating("SpawnRandom", 0.1f, 10f);
     }
     void SpawnRandom()
     {
         // We can't randomize without repeating, but we can remove randomly without repeating :"D
 
-        // List<float> availablePositions = new List<float>() { -4.4f, -3.3f, -2.2f, -1.1f, 0f, 1.1f, 2.2f, 3.3f, 4.4f };
+        //  -4.4f, -3.3f, -2.2f, -1.1f, 0f, 1.1f, 2.2f, 3.3f, 4.4f
 
+        List<float> positions = new List<float>(availablePositions);
         int randomCounter = Random.Range(2, 6);
         for (int i = 0; i <= randomCounter; i++)
-            availablePositions.RemoveAt(Random.Range(0, availablePositions.Count - 1));
+            positions.RemoveAt(Random.Range(0, positions.Count - 1));
 
         GameObject newRow = new GameObject("Row");
         newRow.transform.position = new Vector3(0, 1, 4);
         newRow.AddComponent<Box_Row_Movement>();
-        availablePositions.ForEach(delegate (float position)
+        positions.ForEach(delegate (float position)
         {
             Vector3 randomVector = new Vector3(position, 1, 4);
             Instantiate(boxPrefab, randomVector, Quaternion.identity, newRow.transform);
